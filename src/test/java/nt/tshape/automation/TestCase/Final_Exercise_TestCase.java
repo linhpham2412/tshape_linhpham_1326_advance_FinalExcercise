0c722abce10909646bd15a582488cadf5ed0c8e5
Linh Pham <linh.pham@nashtechglobal.com>
2023-02-20 14:32:16 +0700

    create_testcase_2

--- src/test/java/nt/tshape/automation/TestCase/Final_Exercise_TestCase.java
+++ src/test/java/nt/tshape/automation/TestCase/Final_Exercise_TestCase.java
@@ -35,4 +35,26 @@ public class Final_Exercise_TestCase extends WebDriverTestNGSetupBase {
                 .verifyHotelDetailPageDisplayCorrectHotelLocation()
                 .verifyHotelDetailPageDisplaySelectedBenefitByName("Breakfast included");
     }
+    @SneakyThrows
+    @Test(alwaysRun = true)
+    public void TC_2_Add_Hotel_Into_Favourite_Successfully(){
+        AgodaHomePage agodaHomePage = new AgodaHomePage(getDriver(),getTestContext());
+        AgodaHotelDetailPage agodaHotelDetailPage = new AgodaHotelDetailPage(getDriver(),getTestContext());
+        AgodaSearchPage agodaSearchPage = new AgodaSearchPage(getDriver(),getTestContext());
+
+        //Start Testing
+        agodaHomePage
+                .openAgodaHomePage()
+                .inputSearchLocationByName("Dalat")
+                .selectStartDateFromNextFridayToNextNumberOfDays(3)
+                .selectTravelTypeByName("Family travelers")
+                .changeNumberOfRoomTo(2)
+                .changeNumberOfAdultsTo(4)
+                .clickSearchHotelButton();
+
+        agodaSearchPage
+                .checkSearchResultsNumberShouldContainsLocationValue(5,"Dalat")
+                .filterHotelBenefitWithOptionName("Swimming pool")
+                .clickOnHotelByIndex("1");
+    }
 }
